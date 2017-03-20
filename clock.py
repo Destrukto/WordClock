67aef6d5a7b607c11c14fc0e08e3323054c5fd4c
Destrukto <simon.bielmann@me.com>
2017-03-20 10:03:18 +0100

    Anpassen der Clock für Deutsch und Möglichkeit um via ESC die Clock zu beenden.

--- clock.py
+++ clock.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import os
 import pygame
 import time
@@ -5,15 +7,15 @@ import random
 import math
 from time import strftime, localtime
 from screen import screen
-
+from pygame.locals import *
 
 screen = screen()
 pygame.mouse.set_visible(0)
-font = pygame.font.Font('segoeui.ttf', 90)
+font = pygame.font.Font('segoeui.ttf', 70)
 bkgcolor = (70, 70, 70)
 fgcolor = (255, 255, 255)
 buffer = 50
-circleY = 700
+circleY = 560
 circleRadius = 5
 dots = 20
 
@@ -24,7 +26,7 @@ def dw(texts, ons, y):
         r = font.render(text, True, bkgcolor)
         rs.append(r)
         totalwidth += r.get_width()
-    leftover = (1280 - totalwidth - (buffer * 2)) / len(texts)
+    leftover = (1024 - totalwidth - (buffer * 2)) / len(texts)
     offset = buffer
     for (i, r) in enumerate(rs):
         screen.scr.blit(r, (offset, y))
@@ -33,7 +35,7 @@ def dw(texts, ons, y):
         offset += r.get_width() + leftover
 def drawDotLine():
     second = int(strftime("%S", localtime()))
-    circleSpace = (1280 - (buffer * 2)) / (dots - 1)
+    circleSpace = (1024 - (buffer * 2)) / (dots - 1)
     offset = buffer
     for x in range(0, dots):
         if(second / (60 / dots) >= x):
@@ -41,29 +43,35 @@ def drawDotLine():
         else:
             pygame.draw.circle(screen.scr, bkgcolor, (offset, circleY), circleRadius, 0)
         offset += circleSpace
-
-while 1:
+running = True
+while running:
     screen.scr.fill((0,0,0))
     line1 = 10
-    line2 = 120
-    line3 = 230
-    line4 = 340
-    line5 = 450
-    line6 = 560
+    line2 = 90
+    line3 = 170
+    line4 = 250
+    line5 = 330
+    line6 = 410
     hour = int(strftime("%I", localtime()))
     minute = int(strftime("%M", localtime()))
     if(minute >= 35):
         hour = (1 if hour == 12 else hour + 1)
-    dw(["it", "is", "half", "ten"], [1,1,(1 if minute >= 30 and minute < 35 else 0),
+    dw(["Es", "ist", "halb", "zehn"], [1,1,(1 if minute >= 30 and minute < 35 else 0),
         (1 if (minute >= 10 and minute < 15) or (minute >=50 and minute < 55) else 0)], line1)
-    dw(["quarter", "twenty", "five"], [
+    dw(["viertel", "zwanzig", "fünf"], [
         (1 if minute >= 15 and minute < 20 or minute >= 45 and minute < 50 else 0),
         (1 if minute >= 20 and minute < 30 or minute >= 35 and minute < 45 else 0),
         (1 if minute >= 5 and minute < 10 or minute >= 25 and minute < 30 or minute >= 35 and minute < 40 or minute >= 55 else 0)], line2)
-    dw(["minutes", "to", "past", "one"], [(1 if minute >= 35 and minute < 45 or minute >= 50 else 0),(1 if minute >= 35 else 0),(1 if minute < 35 and minute >= 5 else 0),(1 if hour == 1 else 0)], line3)
-    dw(["three", "two", "four", "five"], [(1 if hour == 3 else 0),(1 if hour == 2 else 0),(1 if hour == 4 else 0),(1 if hour == 5 else 0)], line4)
-    dw(["six", "seven", "eight", "nine"], [(1 if hour == 6 else 0),(1 if hour == 7 else 0),(1 if hour == 8 else 0),(1 if hour == 9 else 0)], line5)
-    dw(["ten", "eleven", "twelve", "o'clock"], [(1 if hour == 10 else 0),(1 if hour == 11 else 0),(1 if hour == 12 else 0),(1 if minute >= 0 and minute < 5 else 0)], line6)
+    dw(["minuten", "vor", "nach", "eins"], [(1 if minute >= 35 and minute < 45 or minute >= 50 else 0),(1 if minute >= 35 else 0),(1 if minute < 30 and minute >= 5 else 0),(1 if hour == 1 else 0)], line3)
+    dw(["drei", "zwei", "vier", "fünf"], [(1 if hour == 3 else 0),(1 if hour == 2 else 0),(1 if hour == 4 else 0),(1 if hour == 5 else 0)], line4)
+    dw(["sechs", "sieben", "acht", "neun"], [(1 if hour == 6 else 0),(1 if hour == 7 else 0),(1 if hour == 8 else 0),(1 if hour == 9 else 0)], line5)
+    dw(["zehn", "elf", "zwölf", "uhr"], [(1 if hour == 10 else 0),(1 if hour == 11 else 0),(1 if hour == 12 else 0),(1 if minute >= 0 and minute < 5 else 0)], line6)
     drawDotLine()
     pygame.display.update()
     time.sleep(1)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            running = False
+pygame.quit()
